47e6122cd2b613845c0652af8566aa398997a705
alvinrach <alvinracht@gmail.com>
2024-12-08 10:54:47 +0000

    V2.py: residual withot projection layer - loss 2.17

--- v2.py
+++ v2.py
@@ -108,8 +108,8 @@ class Block(nn.Module):
         self.ffwd = FeedForward(n_embd)
 
     def forward(self, x):
-        x = self.sa(x)
-        x = self.ffwd(x)
+        x = x + self.sa(x)
+        x = x + self.ffwd(x)
         return x
         
 class BigramLanguageModel(nn.Module):
