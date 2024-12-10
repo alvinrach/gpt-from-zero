376434fb15a68f712c75362193a677258b11f76c
alvinrach <alvinracht@gmail.com>
2024-12-10 21:39:38 +0700

    V2.py: Commenting Some Part

--- v2.py
+++ v2.py
@@ -69,17 +69,19 @@ class Head(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
         B,T,C = x.shape
-        k = self.key(x)
-        q = self.query(x)
+        k = self.key(x) # (B, T, hs)
+        q = self.query(x) # (B, T, hs)
         # compute attention scores ("affinities")
         wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, hs) @ (B, hs, T) -> (B, T, T)
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
         # perform the weighted aggregation of the values
-        v = self.value(x)
-        out = wei @ v
+        v = self.value(x) # (B, T, hs)
+        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
         return out
 
 class MutliHeadAttention(nn.Module):
